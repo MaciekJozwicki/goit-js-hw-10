c9b9f0e2dc6da4e726c16889b45719a72d0741f8
Maciek <maciekjozwicki@wp.pl>
2023-05-11 21:33:01 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+const debounce = require('lodash.debounce')
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+
+
+
+
+inputEl.addEventListener('input',
+    debounce(name => {
+        name = inputEl.value;
+        fetchCountries(name);
+    }, DEBOUNCE_DELAY)
+)
